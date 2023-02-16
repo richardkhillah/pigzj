9155611347ae2ac6d0ed1cff99f5da56c41160f6
Richard Khillah <rkhillah@gmail.com>
2023-02-16 13:56:48 -0800

    getargs from args

---
+++ Args.java
@@ -0,0 +1,33 @@
+import java.lang.NumberFormatException;
+
+public class Args {
+    private int numThreads;
+
+    public Args(String[] args) throws Exception {
+        if( args.length % 2 != 0) {
+            throw new Exception("Invalid argument: Ensure every flag has corresponding value.");
+        }
+
+        // Parse the arguments
+        for(int i = 0; i < args.length; i+=2){
+            switch (args[i]) {
+                case "-p":
+                    numThreads = parseThreads(args[i+1]);
+                    break;
+                default:
+                    throw new Exception("Invalid argument " + args[i]);
+            }    
+        }
+    }
+
+    public int parseThreads(String val) throws NumberFormatException {
+        try {
+            int value = Integer.parseInt(val);
+            return value;
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Parameter -p <int:processes> must be type int.");
+        }
+    }
+
+    public int getThreads() { return numThreads; }
+}
---
+++ Pigzj.java
@@ -0,0 +1,36 @@
+// Imports
+
+// Exception Imports
+import java.io.IOException;
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.lang.Exception;
+
+
+class Pigzj {
+    private int numProcesses;
+
+    public Pigzj() {
+        numProcesses = Runtime.getRuntime().availableProcessors();
+    }
+    public Pigzj(Args a){
+        numProcesses = Runtime.getRuntime().availableProcessors();
+        int temp = a.getThreads();
+        if ((4 * numProcesses ) < temp) {
+            System.err.println("Max Processors Exceeded: Must have less than " + (4 * numProcesses) + " processors.");
+            System.err.println("Setting default processors to " + numProcesses);
+        } else {
+            numProcesses = temp;
+        }
+        System.err.println("numProcessors initialized to " + numProcesses);
+    }
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws Exception, IOException {
+        Pigzj pj = new Pigzj(new Args(args));
+
+        
+    }
+}
